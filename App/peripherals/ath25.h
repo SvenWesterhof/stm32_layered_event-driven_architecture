ffad59b660f8087e29b703708b1adec2abca4f21
SvenWesterhof <svenwesterhof99@gmail.com>
2025-11-20 20:43:12 +0100

    setup ath25 pheripheral library

--- App/peripherals/ath25.h
+++ App/peripherals/ath25.h
@@ -1,39 +1,48 @@
-#ifndef TEMPERATURE_SENSOR_H
-#define TEMPERATURE_SENSOR_H
+#ifndef ATH25_H
+#define ATH25_H
 
 #include "stm32f7xx_hal.h"
 #include "stm32f7xx_hal_i2c.h"
 #include "pinout.h"
+#include <stdbool.h>
 
 // sensor is -40 to 80 degrees with an resolution of 0.01 degrees conversion -> (St / 2^20) * 200 - 50
 
 typedef struct {
+    bool initialized;
+    I2C_HandleTypeDef *hi2c;   // I2C handle
     uint8_t i2c_address;  // I2C address of the temperature sensor
     GPIO_TypeDef *power_port;        // power control pin
     uint16_t power_pin;              // power control pin
     uint8_t resolution;              // sensor resolution (bits)
-    float last_temperature;          // cached value
-} temp_sensor_t;
+} ath25_sensor_t;
 
-temp_sensor_t default_temp_sensor = {
-    .i2c_address = 0x38, // Example I2C address
+ath25_sensor_t default_ath25_sensor = {
+    .initialized = false,
+    .hi2c = NULL,
+    .i2c_address = 0x38 << 1, // Example I2C address
     .power_port = TEMP_SENSOR_ON_OFF_PORT,
     .power_pin = TEMP_SENSOR_ON_OFF_PIN,
-    .resolution = 20, // 20-bit resolution
-    .last_temperature = 0.0f
 };
 
+typedef struct {
+    float temperature;
+    float humidity;
+}ath_data_t;
+
+#define MAX_ATH25_I2C_TRANSFER_TIME 100
+
 // Initialize the temperature sensor (i2nc setup, GPIOs, etc.)
-void temperature_sensor_init();
+void ath25_init();
 
 // Open a connection  and enable the temperature sensor
-void temperature_sensor_open();
+HAL_StatusTypeDef ath25_open(ath25_sensor_t *sensor, I2C_HandleTypeDef *hi2c);
 
 // Read the current temperature value from the sensor
-float temperature_sensor_read(I2C_HandleTypeDef *hi2c, temp_sensor_t *sensor, float *temperature);
+HAL_StatusTypeDef ath25_read(ath25_sensor_t *sensor, ath_data_t *data);
 
 // Close the connection and disable the temperature sensor
-void temperature_sensor_close();
+HAL_StatusTypeDef ath25_close(ath25_sensor_t *sensor);
 
-#endif // TEMPERATURE_SENSOR_H
+#endif // ATH25_H
 
