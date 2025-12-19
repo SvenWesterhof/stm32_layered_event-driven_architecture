3fbb1a6c21212d80b55ab3f836886251615a1414
SvenWesterhof <svenwesterhof99@gmail.com>
2025-12-19 11:15:29 +0100

    implement with different driver

--- App/peripherals/ips_display.c
+++ App/peripherals/ips_display.c
@@ -3,11 +3,12 @@
 #include <stdio.h>
 #include "stm32f7xx_hal.h"
 #include "ips_display.h"
-#include "ST7789/st7789.h"
+#include "ST7735/st7735.h"
 
 
 HAL_StatusTypeDef ips_display_init() {
-    ST7789_Init();
+    ST7735_Init();
+    ST7735_FillScreen(ST7735_RED);
     return HAL_OK;
 }
 
@@ -26,7 +27,7 @@ HAL_StatusTypeDef ips_display_write_temp_data(float temperature, float humidity)
     char hum_str[20];
 
     // Clear the display
-    ST7789_Fill_Color(BLACK);
+    ST7735_FillScreen(ST7735_BLACK);
 
     // Prepare temperature string
     snprintf(temp_str, sizeof(temp_str), "Temp: %.2f C", temperature);
@@ -34,9 +35,9 @@ HAL_StatusTypeDef ips_display_write_temp_data(float temperature, float humidity)
     snprintf(hum_str, sizeof(hum_str), "Hum: %.2f %%", humidity);
 
     // Write temperature to display
-    ST7789_WriteString(10, 30, temp_str, Font_11x18, WHITE, BLACK);
+    ST7735_WriteString(10, 30, temp_str, Font_11x18, ST7735_WHITE, ST7735_BLACK);
     // Write humidity to display
-    ST7789_WriteString(10, 60, hum_str, Font_11x18, WHITE, BLACK);
+    ST7735_WriteString(10, 60, hum_str, Font_11x18, ST7735_WHITE, ST7735_BLACK);
 
     return HAL_OK;
 }
