d80eb60b843306190127b26ed2d9c9a32f91ad4f
SvenWesterhof <svenwesterhof99@gmail.com>
2026-01-12 16:48:35 +0100

    run the current monitor service

--- Middleware/Services/services.c
+++ Middleware/Services/services.c
@@ -2,12 +2,14 @@
 #include "serv_blinky.h"
 #include "serv_temperature_sensor.h"
 #include "serv_display.h"
+#include "serv_current_monitor.h"
 
 void services_init(void)
 {
     blinky_init();
     temperature_sensor_init();
     display_init();
+    current_monitor_init();
 }
 
 void services_run(void)
@@ -15,4 +17,5 @@ void services_run(void)
     blinky_run();
     temperature_sensor_run();
     display_run();
+    current_monitor_process();
 }
