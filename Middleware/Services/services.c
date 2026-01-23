af1d72274c4eff85ffd817e4189d3bbac908003c
SvenWesterhof <svenwesterhof99@gmail.com>
2026-01-23 14:25:43 +0100

    implement ISR based uart and test

--- Middleware/Services/services.c
+++ Middleware/Services/services.c
@@ -5,12 +5,15 @@
 #include "serv_current_monitor.h"
 #include "protocol_handler.h"
 #include "portable_log.h"
+// /#include "hal_uart.h"
+#include "os_wrapper.h"
 
 #ifdef ENABLE_UART_TEST
 #include "../../Tests/uart_test/serv_uart_test.h"
 #endif
 
 static const char *TAG = "SERVICES";
+static uint32_t last_isr_log_time = 0;
 
 void services_init(void)
 {
@@ -47,4 +50,17 @@ void services_run(void)
 #ifdef ENABLE_UART_TEST
     serv_uart_test_loop();
 #endif
+
+    // Debug: Log ISR counters every 5 seconds
+    // TEMPORARILY DISABLED - testing for boot loop
+
+    uint32_t current_time = os_get_tick_count();
+    if (os_ticks_to_ms(current_time - last_isr_log_time) >= 5000) {
+        uint32_t idle_count, dma_ht_count, dma_tc_count;
+        hal_uart_get_isr_counters(&idle_count, &dma_ht_count, &dma_tc_count);
+        LOG_I(TAG, "UART ISR counters: IDLE=%lu, DMA_HT=%lu, DMA_TC=%lu",
+              idle_count, dma_ht_count, dma_tc_count);
+        last_isr_log_time = current_time;
+    }
+
 }
